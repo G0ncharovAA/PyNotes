b96a26b6975e3857ed4a381ab80e402c68e3386d
G0ncharovAA <49533960+G0ncharovAA@users.noreply.github.com>
2023-02-24 16:16:41 +0300

    Урок 1. Приложение заметки (Python)
    Напишите проект, содержащий функционал работы с заметками. Программа должна уметь создавать заметку, сохранять её, читать список заметок, редактировать заметку, удалять заметку.

---
+++ domain/entity/note.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+
+class Note:
+    def __init__(self, id: int, title: str, body: str, date: str):
+        self.id = id
+        self.title = title
+        self.body = body
+        self.date = date
